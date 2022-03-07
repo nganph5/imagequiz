72f79f8a3400a3b43601b21d7709827da62d19c8
Thu <thutra@email.arizona.edu>
2022-03-07 11:58:46 -0700

    Set up

--- imagequiz/src/App.js
+++ imagequiz/src/App.js
@@ -1,52 +1,53 @@
 import React, { Component } from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Home from './components/Home';
-import Register from './components/Register';
-import Login from './components/Login';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Menu from './components/Menu';
+// import { HashRouter, Routes, Route } from 'react-router-dom';
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+// import Home from './components/Home';
+// import Register from './components/Register';
+// import Login from './components/Login';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+// import Menu from './components/Menu';
 
 class App extends Component {
   render() {
     return (
-      <HashRouter>
-        <Container fluid>
-          <Row>
-            <Col>
-            <Header/>
-            </Col>
-          </Row>
+      <div>Hello!!!</div>
+      // <HashRouter>
+      //   <Container fluid>
+      //     <Row>
+      //       <Col>
+      //       <Header/>
+      //       </Col>
+      //     </Row>
   
-          <Row>
-            <Col>
-            <Menu/>
-            </Col>
-          </Row>
+      //     <Row>
+      //       <Col>
+      //       <Menu/>
+      //       </Col>
+      //     </Row>
   
-          <Routes>        
-            <Route exact path='/register' element={<Register />}>
-            </Route>
+      //     <Routes>        
+      //       <Route exact path='/register' element={<Register />}>
+      //       </Route>
             
-            <Route exact path='/login' element={<Login />}>
-            </Route>
+      //       <Route exact path='/login' element={<Login />}>
+      //       </Route>
             
-            <Route exact path='/home' element={<Home />}>
-            </Route>
+      //       <Route exact path='/home' element={<Home />}>
+      //       </Route>
           
-          </Routes>
+      //     </Routes>
   
-          <Row>
-            <Col>
-            <Footer/>
-            </Col>
-          </Row>
+      //     <Row>
+      //       <Col>
+      //       <Footer/>
+      //       </Col>
+      //     </Row>
           
-        </Container>
-      </HashRouter>
+      //   </Container>
+      // </HashRouter>
     );
   }
 }
