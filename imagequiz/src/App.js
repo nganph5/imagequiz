7e609d3b66a381ebf1060d5e16949522538f14e4
Thu <thutra@email.arizona.edu>
2022-03-07 12:00:52 -0700

    Set up

--- imagequiz/src/App.js
+++ imagequiz/src/App.js
@@ -1,55 +1,25 @@
-import React, { Component } from 'react';
-// import { HashRouter, Routes, Route } from 'react-router-dom';
-// import Container from 'react-bootstrap/Container';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
-// import Home from './components/Home';
-// import Register from './components/Register';
-// import Login from './components/Login';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
-// import Menu from './components/Menu';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>Hello!!!</div>
-      // <HashRouter>
-      //   <Container fluid>
-      //     <Row>
-      //       <Col>
-      //       <Header/>
-      //       </Col>
-      //     </Row>
-  
-      //     <Row>
-      //       <Col>
-      //       <Menu/>
-      //       </Col>
-      //     </Row>
-  
-      //     <Routes>        
-      //       <Route exact path='/register' element={<Register />}>
-      //       </Route>
-            
-      //       <Route exact path='/login' element={<Login />}>
-      //       </Route>
-            
-      //       <Route exact path='/home' element={<Home />}>
-      //       </Route>
-          
-      //     </Routes>
-  
-      //     <Row>
-      //       <Col>
-      //       <Footer/>
-      //       </Col>
-      //     </Row>
-          
-      //   </Container>
-      // </HashRouter>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
